cf36f971df343d17b36d231fd49c1743e2205931
YEJINCAU <yeyeye222@cau.ac.kr>
2024-05-30 19:03:52 +0900

    mod: 이슈 통계분석 파싱 및 기간 설정 추가

--- src/components/analysis_issue/LineChart.js
+++ src/components/analysis_issue/LineChart.js
@@ -1,44 +1,32 @@
 import { Line } from 'react-chartjs-2';
-import {Chart,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend } from 'chart.js';
+import { Chart, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip } from 'chart.js';
 import styles from '../../app/analysis_issue/page.module.css';
 
-Chart.register(CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip);
-  
+Chart.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip);
 
-function getDaysInMonth(month, year) { // 달별 날짜수 계산
+function getDaysInMonth(month, year) {
   switch (month) {
-      case '1월': case '3월': case '5월': case '7월':
-      case '8월': case '10월': case '12월':
+    case 1: case 3: case 5: case 7:
+    case 8: case 10: case 12:
       return 31;
-      case '4월': case '6월': case '9월': case '11월':
+    case 4: case 6: case 9: case 11:
       return 30;
-      case '2월':
+    case 2:
       if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
-          return 29;
+        return 29;
       } else {
-          return 28;
+        return 28;
       }
-      default:
-      throw new Error('Invalid month');   
-      }
+    default:
+      throw new Error('Invalid month');
   }
-
-const currentYear = new Date().getFullYear();
+}
 
 export default function LineChart({ data, month }) {
-  const daysInMonth = getDaysInMonth(month, currentYear);
+  const [year, monthNumber] = month.split('-');
+  const numericYear = parseInt(year, 10);
+  const numericMonth = parseInt(monthNumber, 10);
+  const daysInMonth = getDaysInMonth(numericMonth, numericYear);
   const days = Array.from({ length: daysInMonth }, (_, i) => i + 1);
   const issueDays = Object.keys(data).map(Number).filter(day => data[day] > 0);
   const labels = [1, daysInMonth, ...issueDays];
@@ -58,13 +46,18 @@ export default function LineChart({ data, month }) {
   };
 
   const options = {
+    plugins: {
+      legend: {
+        display: false  
+      }
+    },
     scales: {
       x: {
         type: 'category',
         title: { display: true, text: 'Day' },
         ticks: {
-          callback: function(val, index) {
-            return labels.includes(index+1) ? val+1 : '';
+          callback: function (val, index) {
+            return labels.includes(index + 1) ? val + 1 : '';
           },
           autoSkip: false,
           maxRotation: 90,
@@ -89,3 +82,4 @@ export default function LineChart({ data, month }) {
     </div>
   );
 }
+
