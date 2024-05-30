a2aacd6d589a75d7107994ab101a49aeb6ff0b8f
YEJINCAU <yeyeye222@cau.ac.kr>
2024-05-31 01:25:36 +0900

    ftr: api 연동 및 자잘한 수정

--- src/api/LoginAPI.js
+++ src/api/LoginAPI.js
@@ -1,14 +1,21 @@
 import ApiManager from "./apiManager";
 
-export const loginAPI = async () => {
+export const loginAPI = async (identifier, password) => {
   const body = {
-    identifier: "admin",
-    password: "1234",
+    identifier: identifier,
+    password: password,
   };
-  const response = await ApiManager.post("/login", body);
+
+  var response;
+
+  try {
+    response = await ApiManager.post("/login", body);
+  } catch (error) {
+    return false;
+  }
   console.log(response.data);
   localStorage.setItem("token", response.data.token);
   ApiManager.defaults.headers.common["Authorization"] = `Bearer ${response.data.token}`;
   console.log('Header', ApiManager.defaults.headers);
-  return response.data;
+  return response;
 };
