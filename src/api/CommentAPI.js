48758ff9279b49e6d53a8c00857486ce15256cdd
kimju0 <kimuniverse0@naver.com>
2024-06-01 16:41:08 +0900

    ftr: delete/update Comment

--- src/api/CommentAPI.js
+++ src/api/CommentAPI.js
@@ -19,4 +19,24 @@ export const getDetailCommentAPI = async (issueId, commentId) => {
     `/api/issues/${issueId}/comments/${commentId}`
   );
   return response.data;
-}
+};
+
+//코멘트 삭제
+export const deleteCommentAPI = async (issueId, commentId) => {
+  const response = await ApiManager.delete(
+    `/api/issues/${issueId}/comments/${commentId}`
+  );
+  return response.data;
+};
+
+//코멘트 수정
+export const updateCommentAPI = async (issueId, commentId, content) => {
+  const body = {
+    content: content,
+  };
+  const response = await ApiManager.patch(
+    `/api/issues/${issueId}/comments/${commentId}`,
+    body
+  );
+  return response.data;
+};
