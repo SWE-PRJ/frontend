14d7d2475fbedcfd0005877ceb1157e3956e5609
kimju0 <kimuniverse0@naver.com>
2024-06-01 15:35:57 +0900

    ftr: changeIssueStateAPI

--- src/api/IssueAPI.js
+++ src/api/IssueAPI.js
@@ -1,28 +1,51 @@
-import ApiManager from "./apiManager"
+import ApiManager from "./apiManager";
 
 //이슈 생성
-export const createIssueAPI = async (title, description, reporterId, priority, projectId) => {
-    const body = {
-        title: title,
-        description: description,
-        reporterId: reporterId,
-        priority: priority
-    }
-    const response = await ApiManager.post(`/api/projects/${projectId}/issues`, body);
-    // localStorage.setItem("projectID", projectId);
-    console.log("This is createIssueAPI" + response.data);
+export const createIssueAPI = async (
+  title,
+  description,
+  reporterId,
+  priority,
+  projectId
+) => {
+  const body = {
+    title: title,
+    description: description,
+    reporterId: reporterId,
+    priority: priority,
+  };
+  const response = await ApiManager.post(
+    `/api/projects/${projectId}/issues`,
+    body
+  );
+  // localStorage.setItem("projectID", projectId);
+  console.log("This is createIssueAPI" + response.data);
 
-    return response.data;
-}
+  return response.data;
+};
 
 //프로젝트에 속한 이슈 검색
 export const getProjectIssuesAPI = async (projectId) => {
-    const response = await ApiManager.get("/api/projects/" + projectId + "/issues");
-    return response.data;
-}
+  const response = await ApiManager.get(
+    "/api/projects/" + projectId + "/issues"
+  );
+  return response.data;
+};
 
 //할당된 이슈 검색
 export const getUserIssueAPI = async (userId) => {
-    const response = await ApiManager.get("/api/users/" + userId + "/issues");
-    return response.data;
-}
+  const response = await ApiManager.get("/api/users/" + userId + "/issues");
+  return response.data;
+};
+
+//이슈 상태 변경
+export const changeIssueStateAPI = async (projectId, issueId, state) => {
+  const body = {
+    state: state,
+  };
+  const response = await ApiManager.patch(
+    `/api/projects/${projectId}/issues/${issueId}`,
+    body
+  );
+  return response.data;
+};
